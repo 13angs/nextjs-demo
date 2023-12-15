333fd11792c868ed84afde77a7dcf25e21e93796
13angs <13angz.um@gmail.com>
2023-12-15 15:29:44 +0000

    add gh action

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,11 +1,16 @@
-// pages/chat.js
+// pages/chat.tsx
 "use client"
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, ChangeEvent } from 'react';
 
-const Chat = () => {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState('');
-  const chatContainerRef = useRef(null);
+interface Message {
+  text: string;
+  timestamp: number;
+}
+
+const Chat: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [newMessage, setNewMessage] = useState<string>('');
+  const chatContainerRef = useRef<HTMLDivElement>(null);
 
   const sendMessage = () => {
     if (newMessage.trim() === '') return;
@@ -16,7 +21,9 @@ const Chat = () => {
 
   useEffect(() => {
     // Scroll to the bottom when a new message is added
-    chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
   }, [messages]);
 
   return (
@@ -26,19 +33,16 @@ const Chat = () => {
         className="flex-1 overflow-y-scroll p-4"
         style={{ scrollBehavior: 'smooth' }}
       >
-        {messages.map((message, index) => {
-          const ind = index
-          return (
-            <div
-              key={ind}
-              className={`mb-2 p-3 rounded-md ${
-                index % 2 === 0 ? 'bg-blue-500 text-white' : 'bg-gray-100 text-gray-800'
-              } shadow-md`}
-            >
-              <p>{message.text}</p>
-            </div>
-          )
-        })}
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className={`mb-2 p-3 rounded-md ${
+              index % 2 === 0 ? 'bg-blue-500 text-white' : 'bg-gray-100 text-gray-800'
+            } shadow-md`}
+          >
+            <p>{message.text}</p>
+          </div>
+        ))}
       </div>
       <div className="p-4 flex items-center bg-white border-t border-gray-200 shadow-md">
         <input
@@ -46,7 +50,7 @@ const Chat = () => {
           className="flex-1 border rounded p-3 mr-2 focus:outline-none text-gray-800"
           placeholder="Type a message..."
           value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setNewMessage(e.target.value)}
         />
         <button
           className="bg-blue-500 text-white px-6 py-3 rounded"
